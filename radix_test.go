2b0fbe5ed9c61f823d7282f70086e3662c6353bf
s.kamardin <s.kamardin@corp.mail.ru>
2016-10-05 22:19:16 +0300

    pairs -> path

--- radix_test.go
+++ radix_test.go
@@ -6,8 +6,10 @@ import (
 	"testing"
 )
 
+type pairs []Pair
+
 type item struct {
-	p Pairs
+	p pairs
 	v int
 }
 
@@ -19,47 +21,47 @@ type del struct {
 func TestTrieInsertLookup(t *testing.T) {
 	for i, test := range []struct {
 		insert []item
-		lookup []Pairs
+		lookup []pairs
 		expect []int
 	}{
 		{
 			insert: []item{
-				{Pairs{{1, "a"}, {2, "b"}}, 1},
+				{pairs{{1, "a"}, {2, "b"}}, 1},
 			},
-			lookup: []Pairs{
-				Pairs{{1, "a"}, {2, "b"}},
-				Pairs{{2, "b"}, {1, "a"}},
+			lookup: []pairs{
+				pairs{{1, "a"}, {2, "b"}},
+				pairs{{2, "b"}, {1, "a"}},
 			},
 			expect: []int{1},
 		},
 		{
 			insert: []item{
-				{Pairs{{1, "a"}, {2, "b"}}, 1},
-				{Pairs{{1, "a"}, {2, "b"}}, 2},
-				{Pairs{{1, "a"}}, 3},
-				{Pairs{{2, "b"}}, 4},
-				{Pairs{}, 5},
+				{pairs{{1, "a"}, {2, "b"}}, 1},
+				{pairs{{1, "a"}, {2, "b"}}, 2},
+				{pairs{{1, "a"}}, 3},
+				{pairs{{2, "b"}}, 4},
+				{pairs{}, 5},
 			},
-			lookup: []Pairs{
-				Pairs{{1, "a"}, {2, "b"}},
-				Pairs{{2, "b"}, {1, "a"}},
+			lookup: []pairs{
+				pairs{{1, "a"}, {2, "b"}},
+				pairs{{2, "b"}, {1, "a"}},
 			},
 			expect: []int{1, 2, 3, 4, 5},
 		},
 	} {
 		trie := New()
 		for _, op := range test.insert {
-			trie.Insert(op.p, op.v)
+			trie.Insert(PathFromSlice(op.p...), op.v)
 		}
 
-		for _, path := range test.lookup {
+		for _, p := range test.lookup {
 			var result []int
-			trie.Lookup(path, func(v int) bool {
+			trie.Lookup(PathFromSlice(p...), func(v int) bool {
 				result = append(result, v)
 				return true
 			})
 			if !listEq(result, test.expect) {
-				t.Errorf("[%d] Lookup(%v) = %v; want %v", i, path, result, test.expect)
+				t.Errorf("[%d] Lookup(%v) = %v; want %v", i, p, result, test.expect)
 			}
 		}
 	}
@@ -73,47 +75,47 @@ func TestTrieInsertDelete(t *testing.T) {
 	}{
 		{
 			insert: []item{
-				{Pairs{{1, "a"}, {2, "b"}}, 1},
+				{pairs{{1, "a"}, {2, "b"}}, 1},
 			},
 			delete: []del{
-				{item{Pairs{{1, "a"}, {2, "b"}}, 1}, true},
+				{item{pairs{{1, "a"}, {2, "b"}}, 1}, true},
 			},
 			expect: []int{},
 		},
 		{
 			insert: []item{
-				{Pairs{{1, "a"}, {2, "b"}}, 1},
-				{Pairs{{1, "a"}, {2, "b"}}, 2},
-				{Pairs{{1, "a"}}, 3},
-				{Pairs{{2, "b"}}, 4},
-				{Pairs{}, 5},
+				{pairs{{1, "a"}, {2, "b"}}, 1},
+				{pairs{{1, "a"}, {2, "b"}}, 2},
+				{pairs{{1, "a"}}, 3},
+				{pairs{{2, "b"}}, 4},
+				{pairs{}, 5},
 			},
 			delete: []del{
-				{item{Pairs{{1, "a"}, {2, "b"}}, 1}, true},
-				{item{Pairs{{1, "a"}}, 3}, true},
-				{item{Pairs{{1, "a"}}, 4}, false},
+				{item{pairs{{1, "a"}, {2, "b"}}, 1}, true},
+				{item{pairs{{1, "a"}}, 3}, true},
+				{item{pairs{{1, "a"}}, 4}, false},
 			},
 			expect: []int{2, 4, 5},
 		},
 	} {
 		trie := New()
 		for _, op := range test.insert {
-			trie.Insert(op.p, op.v)
+			trie.Insert(PathFromSlice(op.p...), op.v)
 		}
 		for _, del := range test.delete {
-			if del.ok != trie.Delete(del.p, del.v) {
+			if del.ok != trie.Delete(PathFromSlice(del.p...), del.v) {
 				t.Errorf("[%d] Delete(%v, %v) = %v; want %v", i, del.p, del.v, !del.ok, del.ok)
 			}
 		}
 		var result []int
-		trie.Lookup(nil, func(v int) bool {
+		trie.Lookup(PathFromSlice(), func(v int) bool {
 			result = append(result, v)
 			return true
 		})
 		if !listEq(result, test.expect) {
 			t.Errorf(
 				"[%d] after Delete; Lookup(%v) = %v; want %v",
-				i, Pairs{}, result, test.expect,
+				i, pairs{}, result, test.expect,
 			)
 		}
 	}
@@ -157,20 +159,20 @@ func benchmarkInsert(b *testing.B, exists, n int) {
 	t := New()
 	values := randStr(exists+n, 16)
 	for i := 0; i < exists; i++ {
-		t.Insert(Pairs{{1, values[i]}}, v)
+		t.Insert(PathFromSlice(Pair{1, values[i]}), v)
 		v++
 	}
 	insert := values[exists:]
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		for j := 0; j < len(insert); j++ {
-			t.Insert(Pairs{{1, insert[j]}}, v)
+			t.Insert(PathFromSlice(Pair{1, insert[j]}), v)
 			v++
 		}
 		b.StopTimer()
 		for j := len(insert) - 1; j >= 0; j-- {
 			v--
-			if !t.Delete(Pairs{{1, insert[j]}}, v) {
+			if !t.Delete(PathFromSlice(Pair{1, insert[j]}), v) {
 				b.Fatalf("could not delete previously inserted element")
 			}
 		}
