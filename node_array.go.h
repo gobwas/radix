904e03e9cd45ab966d32a4aaf39810c960446ae0
gobwas <gobwas@gmail.com>
2016-10-12 00:46:20 +0300

    regenerate

--- node_array.go.h
+++ node_array.go.h
@@ -1,10 +1,11 @@
-#include "array.h"
+#include "sync_array.h"
 
 #define ID(a) a.key
 #define LESS_OR_EQUAL(a, b) a.key <= b.key
 #define GREATER(a, b) a.key > b.key
 #define FUNC(a) node##a
 #define STRUCT(a) node##a
+#define EMPTY() nil
 
 package radix
 
