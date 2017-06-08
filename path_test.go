82dc28e0130770e4d52b67bd3b79817f1dbe5d19
Sergey Kamardin <s.kamardin@corp.mail.ru>
2017-06-08 13:03:36 +0300

    benchmarks refactoring

--- path_test.go
+++ path_test.go
@@ -99,58 +99,78 @@ func TestPathFirst(t *testing.T) {
 	}
 }
 
-func benchmarkPathFromMap(b *testing.B, size int) {
-	m := make(map[uint]string, size)
-	s := randStr(size)
-	for i := 0; i < size; i++ {
-		m[uint(i)] = s[i]
-	}
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		_ = PathFromMap(m)
+var sizes = []int{2, 4, 6, 8, 10, 16, 32}
+
+func BenchmarkPathFromMap(b *testing.B) {
+	for _, size := range sizes {
+		b.Run(fmt.Sprintf("%d", size), func(b *testing.B) {
+			m := make(map[uint]string, size)
+			s := randStr(size)
+			for i := 0; i < size; i++ {
+				m[uint(i)] = s[i]
+			}
+			b.ResetTimer()
+
+			for i := 0; i < b.N; i++ {
+				_ = PathFromMap(m)
+			}
+		})
 	}
 }
 
-func benchmarkPathFromSlice(b *testing.B, size int) {
-	data := make([]Pair, size)
-	s := randStr(size)
-	for i, key := range rand.Perm(size) {
-		data[i] = Pair{uint(key), s[i]}
-	}
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		_ = PathFromSlice(data)
+func BenchmarkPathFromSlice(b *testing.B) {
+	for _, size := range sizes {
+		b.Run(fmt.Sprintf("%d", size), func(b *testing.B) {
+			data := make([]Pair, size)
+			s := randStr(size)
+			for i, key := range rand.Perm(size) {
+				data[i] = Pair{uint(key), s[i]}
+			}
+			b.ResetTimer()
+
+			for i := 0; i < b.N; i++ {
+				_ = PathFromSlice(data)
+			}
+		})
 	}
 }
 
-func benchmarkPathWithout(b *testing.B, size int) {
+func BenchmarkPathWithout(b *testing.B) {
+	for _, size := range sizes {
+		b.Run(fmt.Sprintf("%d", size), func(b *testing.B) {
+			path := makePath(size)
+			rid := rand.Perm(size)
+			b.ResetTimer()
+
+			for i := 0; i < b.N; i++ {
+				_ = path.Without(uint(rid[i%size]))
+			}
+		})
+	}
+}
+
+func doSomeFunc(it func() (uint, bool)) {
+	for ok := true; ok; _, ok = it() {
+	}
+}
+
+func BenchmarkPathAscendKeyIterator(b *testing.B) {
+	for _, size := range sizes {
+		path := makePath(size)
+		b.ResetTimer()
+
+		for i := 0; i < b.N; i++ {
+			doSomeFunc(path.AscendKeyIterator())
+		}
+	}
+}
+
+func makePath(size int) Path {
 	data := make([]Pair, size)
 	s := randStr(size)
 	rid := rand.Perm(size)
 	for i, key := range rid {
 		data[i] = Pair{uint(key), s[i]}
 	}
-	path := PathFromSlice(data)
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		_ = path.Without(uint(rid[i%size]))
-	}
+	return PathFromSlice(data)
 }
-
-func BenchmarkPathWithout_2(b *testing.B)  { benchmarkPathWithout(b, 2) }
-func BenchmarkPathWithout_4(b *testing.B)  { benchmarkPathWithout(b, 4) }
-func BenchmarkPathWithout_8(b *testing.B)  { benchmarkPathWithout(b, 8) }
-func BenchmarkPathWithout_16(b *testing.B) { benchmarkPathWithout(b, 16) }
-func BenchmarkPathWithout_32(b *testing.B) { benchmarkPathWithout(b, 32) }
-
-func BenchmarkPathFromMap_2(b *testing.B)  { benchmarkPathFromMap(b, 2) }
-func BenchmarkPathFromMap_4(b *testing.B)  { benchmarkPathFromMap(b, 4) }
-func BenchmarkPathFromMap_8(b *testing.B)  { benchmarkPathFromMap(b, 8) }
-func BenchmarkPathFromMap_16(b *testing.B) { benchmarkPathFromMap(b, 16) }
-func BenchmarkPathFromMap_32(b *testing.B) { benchmarkPathFromMap(b, 32) }
-
-func BenchmarkPathFromSlice_2(b *testing.B)  { benchmarkPathFromSlice(b, 2) }
-func BenchmarkPathFromSlice_4(b *testing.B)  { benchmarkPathFromSlice(b, 4) }
-func BenchmarkPathFromSlice_8(b *testing.B)  { benchmarkPathFromSlice(b, 8) }
-func BenchmarkPathFromSlice_16(b *testing.B) { benchmarkPathFromSlice(b, 16) }
-func BenchmarkPathFromSlice_32(b *testing.B) { benchmarkPathFromSlice(b, 32) }
